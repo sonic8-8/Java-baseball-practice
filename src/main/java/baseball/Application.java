bd942915f8659bb3a3ffe20b8918a5ef9f2aab5c
이승환 <154510650+sonic8-8@users.noreply.github.com>
2024-10-25 05:12:36 +0900

    Refactor/io (#2)
    * ConsoleOutputHandler 객체 분리
    
    * ConsoleInputHandler 객체 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,84 +1,110 @@
 package baseball;
 
+import baseball.io.ConsoleInputHandler;
+import baseball.io.ConsoleOutputHandler;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Application {
+
+    private static final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private static final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+
+    private static int strike = 0;
+    private static int ball = 0;
+    private static int gameStatus = 0;
+
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        consoleOutputHandler.showStartGameComment();
+
         Random random = new Random();
-
-        int strikeNum = 0;
-        int ballNum = 0;
-        int gameStatus = 0; // 이기면 1로 변경, 기본 상태는 0
-//        int exceptionStatus = 0; // 1이면 예외 발생한 것
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        int random1 = random.nextInt(9) + 1;
+        int random1 = getRandomOneDigitNumber(random);
         int random2 = random.nextInt(9) + 1;
         int random3 = random.nextInt(9) + 1;
 
-        System.out.println(random1 + " " + random2 + " " + random3);
+        while (true) {
 
-        while (gameStatus == 0) {
+            int threeDigitNumber = getThreeDigitNumberFromUser();
 
-            System.out.print("숫자를 입력해주세요 : ");
-            int number = scanner.nextInt();
+            if (threeDigitNumber < 100 || threeDigitNumber > 999) {
+                throw new IllegalArgumentException();
+            }
 
-            int number1 = number / 100;
-            int number2 = (number - number1 * 100) / 10;
-            int number3 = number % 10;
+            int number1 = threeDigitNumber / 100;
+            int number2 = (threeDigitNumber - number1 * 100) / 10;
+            int number3 = threeDigitNumber % 10;
 
             if (number1 == number2 || number1 == number3 || number2 == number3) {
                 throw new IllegalArgumentException();
             }
 
-            if (number < 100 || number > 999) {
-                throw new IllegalArgumentException();
-            }
+            countStrikeAndBall(number1, random1, random2, random3, number2, number3);
 
-            if (number1 == random1) {
-                strikeNum++;
-            } else if (number1 == random2 || number1 == random3) {
-                ballNum++;
-            }
+            consoleOutputHandler.showGameHint(strike, ball);
 
-            if (number2 == random2) {
-                strikeNum++;
-            } else if (number2 == random1 || number2 == random3) {
-                ballNum++;
-            }
-
-            if (number3 == random3) {
-                strikeNum++;
-            } else if (number3 == random1 || number3 == random2) {
-                ballNum++;
-            }
-
-            if (strikeNum == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            if (isWinningStrike()) {
+                consoleOutputHandler.printGameWinningComment();
                 gameStatus = 1;
-            } else if (strikeNum != 0) {
-                System.out.println(strikeNum + "스트라이크");
-            } else if (ballNum != 0) {
-                System.out.println(ballNum + "볼");
-            } else {
-                System.out.println("낫싱");
+                break;
             }
 
-            strikeNum = 0;
-            ballNum = 0;
+            strike = 0;
+            ball = 0;
 
             if (gameStatus == 1) {
-
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int select = scanner.nextInt();
-
-                if (select == 1) {
+                int userAction = getUserActionFromUser();
+                
+                if (doesUserChooseToContinue(userAction)) {
                     gameStatus = 0;
+                    break;
                 }
             }
+        }
+    }
+
+    private static int getRandomOneDigitNumber(Random random) {
+        int random1 = random.nextInt(9) + 1;
+        return random1;
+    }
+
+    private static int getUserActionFromUser() {
+        consoleOutputHandler.printCommentForUserAction();
+        return consoleInputHandler.getUserInput();
+    }
+
+    private static int getThreeDigitNumberFromUser() {
+        consoleOutputHandler.printCommentForThreeDigitNumber();
+        return consoleInputHandler.getUserInput();
+    }
+
+    private static boolean doesUserChooseToContinue(int select) {
+        return select == 1;
+    }
+
+    private static boolean isWinningStrike() {
+        return strike == 3;
+    }
+
+    private static void countStrikeAndBall(int number1, int random1, int random2, int random3, int number2, int number3) {
+        if (number1 == random1) {
+            strike++;
+        } else if (number1 == random2 || number1 == random3) {
+            ball++;
+        }
+
+        if (number2 == random2) {
+            strike++;
+        } else if (number2 == random1 || number2 == random3) {
+            ball++;
+        }
+
+        if (number3 == random3) {
+            strike++;
+        } else if (number3 == random1 || number3 == random2) {
+            ball++;
         }
     }
 }
---
+++ src/main/java/baseball/io/ConsoleOutputHandler.java
@@ -0,0 +1,34 @@
+package baseball.io;
+
+public class ConsoleOutputHandler {
+
+    public void showStartGameComment() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void showGameHint(int strike, int ball) {
+        if (0 < strike && strike < 3) {
+            System.out.println(strike + "스트라이크");
+        }
+
+        if (0 < ball && ball < 4) {
+            System.out.println(ball + "볼");
+        }
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    public void printGameWinningComment() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printCommentForUserAction() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printCommentForThreeDigitNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
